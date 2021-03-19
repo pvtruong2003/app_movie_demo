fc3b21d5958ab91a50342e494818a169374bf6b8
Bao Ngo <bao.ngo@bstarsolutions.com>
2021-03-19 17:04:44 +0700

    - update ui tab profile

--- app_movie/lib/screens/profile/profile_sreen.dart
+++ app_movie/lib/screens/profile/profile_sreen.dart
@@ -1,4 +1,6 @@
+import 'package:app_movie/app_bar.dart';
 import 'package:app_movie/app_container.dart';
+import 'package:app_movie/screens/profile/Components/body.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -11,9 +13,9 @@ class _ProfileScreenState extends State<ProfileScreen> with AutomaticKeepAliveCl
   Widget build(BuildContext context) {
     print('---------------> ProfileScreen');
     super.build(context);
-    return AppContainer(
-      appBar: AppBar(),
-      child: Container(),
+    return Scaffold(
+      // appBar: customAppBar(title: "Profile User", centerTitle: true),
+      body: Body(),
     );
   }
 
